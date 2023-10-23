d81a19af9c7d6fec1a004791fac602464aab03ca
Shashank Srivastav <shashankgrovy@gmail.com>
2023-10-23 16:14:10 +0530

    Adds pydantic settings to load settings from envs

--- shortest/v1/mutator.py
+++ shortest/v1/mutator.py
@@ -5,16 +5,16 @@ from pydantic import BaseModel, Field
 
 
 class Payload(BaseModel):
-    """Payload for shortening a URL"""
+    '''Payload for shortening a URL'''
     source_url: str
     custom_hex: Optional[str] = Field(
         default=None,
         max_length=10,
-        description="A custom url key to be used instead of hash"
+        description='A custom url key to be used instead of hash'
     )
 
 
 class Response(BaseModel):
-    """A default response class"""
+    '''A default response class'''
     status: HTTPStatus
     message: Any
--- shortest/v1/processor.py
+++ shortest/v1/processor.py
@@ -1,10 +1,11 @@
 import hashlib
 
+from shortest.config import get_settings
 from .mutator import Payload
 
 
 def _encode(url: str, length: int = 5, offset: int = 0) -> str:
-    """Encodes a given url string using md5 and returns a hex of
+    '''Encodes a given url string using md5 and returns a hex of
     given length
 
     Args:
@@ -14,18 +15,19 @@ def _encode(url: str, length: int = 5, offset: int = 0) -> str:
 
     Returns:
         str: A hex digest of given length
-    """
+    '''
     hex = hashlib.md5(url.encode()).hexdigest()
     return hex[offset:length]
 
 
 def encoder(payload: Payload) -> str:
-    """Encodes a given url and returns it"""
+    '''Encodes a given url and returns it'''
 
+    settings = get_settings()
     if payload.custom_hex:
-        hex = payload.custom_hex
-        return hex
+        payload.custom_hex
+        return f'{settings.base_url}/{payload.custom_hex}'
 
     # Proceed with encoding the url
     hex = _encode(payload.source_url)
-    return hex
+    return f'{settings.base_url}/{hex}'
